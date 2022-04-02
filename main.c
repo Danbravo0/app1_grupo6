d979d11755714b90394997e0dcb9ca57d6f6b9cf
Daniel Bravo <101165561+Danbravo0@users.noreply.github.com>
2022-04-02 10:34:58 -0300

    Actualización
    agregada el "to_delete" para generar baja logica

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include <stdlib.h>
 #include <string.h>
-#include<stdbool.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <libros.h>
+
 
 int menu(); //Protipo menu
 // void guardado();//Prototipo del algoritmo usado para guardar los libros en arrays
@@ -17,7 +18,8 @@ int menu(); //Protipo menu
 void printValues(Libro libros[]){
 
     // printf("hola si con quien ");
-    for (int i=0;i<15;i++){
+
+    for (int i=0;i<10;i++){
         // printf("hola si con quien ");
         printf("titulo = %s, " ,libros[i].titulo );
         printf("autor = %s, " ,libros[i].autor  );
@@ -27,7 +29,7 @@ void printValues(Libro libros[]){
         printf("piso = %s, " ,libros[i].piso );
         printf("edificio = %s, " ,libros[i].edificio );
         printf("sede = %s, " ,libros[i].sede );
-
+        printf( "delete = %d ", libros[i].to_delete);
 
 
 
@@ -64,84 +66,53 @@ int main(int argc, char *argv[]){
         if(contador_fila == 1){
             continue; //No nos importa la primer fila, ya que son los nombres de cada columna
         }
+
+
         char *campo = strtok(buff,","); //Separa una string en una serie de "tokens" usando el delimitador coma https://www.tutorialspoint.com/c_standard_library/c_function_strtok.htm
-    // printf("wena");
+
         while (campo){
-    // printf("wena\n");
+
              switch(contador_campo){
-                 case 0:
-                 strcpy(libros[i].titulo,campo);
-                 break;
-                //  printf("0 = %s \n",libros[i].titulo);
-
-                 case 1:
-                 strcpy(libros[i].autor,campo);
-                 break;
-                 //  printf("1 = %s \n",libros[i].autor);
-
-                 case 2:
-                 strcpy(libros[i].anio,campo);
-                 break;
-                //  printf("2 = %d \n",libros[i].anio);
-                case 3:
-                 strcpy(libros[i].estante_numero,campo);
+                 case 0: strcpy(libros[i].titulo,campo);
                  break;
 
-                 case 4:
-                 strcpy(libros[i].estante_seccion,campo);
+                 case 1: strcpy(libros[i].autor,campo);
                  break;
 
-                 case 5:
-                 strcpy(libros[i].piso,campo);
+                 case 2: strcpy(libros[i].anio,campo);
                  break;
 
-                 case 6:
-                 strcpy(libros[i].edificio,campo);
+                case 3: strcpy(libros[i].estante_numero,campo);
                  break;
 
-                 case 7:
-                 strcpy(libros[i].sede,campo);
+                 case 4: strcpy(libros[i].estante_seccion,campo);
                  break;
+
+                 case 5: strcpy(libros[i].piso,campo);
+                 break;
+
+                 case 6: strcpy(libros[i].edificio,campo);
+                 break;
+
+                 case 7: strcpy(libros[i].sede,campo);
+                 break;
+
+                 default:
+                     break;
              }
              campo = strtok(NULL,","); //actualizar valor del campo
              contador_campo ++;
         }
+        libros[i].to_delete=0;
         i++;
-    // printf("wena");
     }
     fclose(fp);
 
+
+    //borrar_libro(libros);
+
     printValues(libros);
     printf("wenarda 2");
+    //infoLibro(libros[]);
     return 0;
-}
-
-
-
-
-
-int menu(){
-
-    // do{
-        int x;
-        printf("\n**************************************\n");
-        printf("\nBienvenido al inventario de libros UAI\n");
-        printf("Ingrese uno de los siguientes numeros para hacer la accion a su derecha\n");
-        printf("1.  Buscar un libro\n");
-        printf("2.  Agregar un libro nuevo\n");
-        printf("3.  Quitar un libro existente\n");
-        printf("4.  Agregar una sede\n");
-        printf("5.  Quitar una sede\n"); //Solo si no tiene libros asociados
-        printf("6.  Editar un libro\n");
-        printf("7.  Cambiar un libro de sede\n");
-        printf("8.  Cambiar un libro de seccion\n");
-        printf("9.  Cambiar un libro de piso \n");
-        printf("10. Agregar secciones\n");
-        printf("11. Eliminar secciones\n"); //Solo si no tiene libros asociados
-        printf("12. Agregar pisos\n");
-        printf("13. Quitar pisos\n"); //Solo si no tiene libros asociados
-        printf("\n**************************************\n");
-        scanf("%d", &x);
-        return(x);
-    // }while(true);
 }
