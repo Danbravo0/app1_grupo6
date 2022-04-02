a3d4f485d0e3abd92c96055503784693bb143327
Daniel Bravo <101165561+Danbravo0@users.noreply.github.com>
2022-04-02 10:42:11 -0300

    Update main.c

--- main.c
+++ main.c
@@ -116,3 +116,29 @@ int main(int argc, char *argv[]){
     //infoLibro(libros[]);
     return 0;
 }
+
+int menu(){
+
+    // do{
+        int x;
+        printf("\n**************************************\n");
+        printf("\nBienvenido al inventario de libros UAI\n");
+        printf("Ingrese uno de los siguientes numeros para hacer la accion a su derecha\n");
+        printf("1.  Buscar un libro\n");
+        printf("2.  Agregar un libro nuevo\n");
+        printf("3.  Quitar un libro existente\n");
+        printf("4.  Agregar una sede\n");
+        printf("5.  Quitar una sede\n"); //Solo si no tiene libros asociados
+        printf("6.  Editar un libro\n");
+        printf("7.  Cambiar un libro de sede\n");
+        printf("8.  Cambiar un libro de seccion\n");
+        printf("9.  Cambiar un libro de piso \n");
+        printf("10. Agregar secciones\n");
+        printf("11. Eliminar secciones\n"); //Solo si no tiene libros asociados
+        printf("12. Agregar pisos\n");
+        printf("13. Quitar pisos\n"); //Solo si no tiene libros asociados
+        printf("\n**************************************\n");
+        scanf("%d", &x);
+        return(x);
+    // }while(true);
+}
